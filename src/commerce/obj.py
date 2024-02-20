9e6a71ca107383e48d1c0ddcc5c8688f2d8f3907
SRE <Nerocross507@gmail.com>
2024-02-20 15:55:36 +0300

    Task 1 Completed (hot fix)

--- src/commerce/obj.py
+++ src/commerce/obj.py
@@ -12,12 +12,14 @@ class Category:
         self.description = description
         self.__products = products
 
-    def add_product(self, dict_category: list):
-        if dict_category.get('name') == self.name:
-            return f"Извините, категория '{dict_category.get('name')}' уже присутствует в списке!"
-        self.__products.update(dict_category)
-        # print(self.__products)
-
+    def add_product(self, list_category: list):
+        for category in list_category:
+            # print(category)
+            if category.get("name") == self.name:
+                # print(self.__products)
+                return f"Извините, категория '{category.get('name')}' уже присутствует в списке!"
+            self.__products.append(list_category)
+            print(self.__products)
 
 
 class Product:
@@ -36,36 +38,35 @@ class Product:
         self.quantity = quantity
 
 
-cat1 = Category('Чай', 'Черный', {
-        "name": "Майский",
-        "description": "Нормальный пацанский чай",
-        "price": 300.0,
-        "quantity": 7
-        })
+cat1 = Category("Чай", "Черный", [{
+    "name": "Майский",
+    "description": "Нормальный пацанский чай",
+    "price": 300.0,
+    "quantity": 7
+}])
 
-add_data = {'name': 'Чай', 'description': 'Черный', 'products': {
-        "name": "Майский",
-        "description": "Нормальный пацанский чай",
-        "price": 300.0,
-        "quantity": 7
-}}
+add_data = [{"name": "Чай", "description": "Черный", "products": {
+    "name": "Майский",
+    "description": "Нормальный пацанский чай",
+    "price": 300.0,
+    "quantity": 7
+}}]
 
-add_data1 = {'name': 'Кофе', 'description': 'Черный гранулированный', 'products': [{
-        "name": "Nescafe",
-        "description": "Нормальный пацанский кофе",
-        "price": 300.0,
-        "quantity": 7
-        }, {
+add_data1 = [{"name": "Кофе", "description": "Черный гранулированный", "products": [{
+    "name": "Nescafe",
+    "description": "Нормальный пацанский кофе",
+    "price": 300.0,
+    "quantity": 7
+}, {
     "name": "365 дней",
     "description": "Ну такой себе кофеек, но не помрёшь",
     "price": 100.0,
     "quantity": 3
-    }]
-}
-
-#cat1.add_product(add_data1)
+}]
+             }]
 
 
+cat1.add_product(add_data1)
 
 # for products in list_category:
 #     if products.name == self.name:
