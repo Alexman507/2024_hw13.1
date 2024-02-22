3cdad7175b3e63f34e68ffcec9a00923f171ce1f
SRE <Nerocross507@gmail.com>
2024-02-22 23:09:20 +0300

    moved dir test for best practics

--- tests/test_func_json.py
+++ tests/test_func_json.py
@@ -2,7 +2,7 @@ from src.commerce import func_json
 
 
 def test_json_loads():
-    data_category = func_json.json_loads('../../data/products.json')
+    data_category = func_json.json_loads('../tests/test_data/test_products.json')
     assert len(data_category) > 0
     assert len(data_category) == 2
     assert type(data_category) == list
